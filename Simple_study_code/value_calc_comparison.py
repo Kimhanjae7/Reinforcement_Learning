2bf6c0f800aefe20d974059aa3fe53409aea9a22
Kimhanjae7 <hjk2512@naver.com>
2025-01-21 16:17:23 +0900

    value_calc

--- Simple_study_code/value_calc_comparison.py
+++ Simple_study_code/value_calc_comparison.py
@@ -2,14 +2,14 @@
 gamma = 0.9  # 감가율
 rewards = [1, 1, 1, 1, 1]  # 단순히 매 시점 보상이 1인 경우
 
-# 첫 번째 식: 무한 시점 누적 보상 계산
+# 무한 시점 누적 보상 계산
 def calculate_value_directly(rewards, gamma):
     V = 0
     for t, reward in enumerate(rewards):
         V += (gamma ** t) * reward
     return V
 
-# 두 번째 식: 벨만 기대 방정식으로 계산
+# 벨만 기대 방정식으로 계산
 def calculate_value_recursively(rewards, gamma, t=0):
     if t >= len(rewards):
         return 0
